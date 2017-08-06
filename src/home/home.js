c71734c7e5138ce8406e75fd87150c53de05d017
kusob <boseokjung@gmail.com>
2017-08-06 19:37:57 +0900

    some style

--- src/home/home.js
+++ src/home/home.js
@@ -1,12 +1,12 @@
 /**
  * Created by kusob on 2017. 6. 26..
  */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     ScrollView, StyleSheet, Alert,
     Text, AsyncStorage, View, TouchableOpacity,
 } from 'react-native';
-import {Actions} from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 import PrivateAddr from "../common/private/address";
 import Common from "../common/common";
 import LoadingIcon from "../common/loadingIcon";
@@ -31,7 +31,7 @@ class Home extends Component {
         const tokens = await AsyncStorage.getItem('Token');
         const token = JSON.parse(tokens).token;
         console.log(token);
-        this.setState({token: token}, () => {
+        this.setState({ token: token }, () => {
             this.getMynickname();
             this.getConfirm();
             this.getConfirmMyside();
@@ -48,7 +48,7 @@ class Home extends Component {
         })
             .then((response) => response.json())
             .then((responseJson) => {
-                this.setState({nickname: responseJson.nickname, nicknameLoad: true});
+                this.setState({ nickname: responseJson.nickname, nicknameLoad: true });
             })
             .catch((error) => {
                 alert('Network Connection Fail : ' + error);
@@ -66,7 +66,7 @@ class Home extends Component {
             .then((response) => response.json())
             .then((responseJson) => {
                 if (responseJson.message == "SUCCESS") {
-                    this.setState({confirmList: responseJson.list, confirmLoad: true});
+                    this.setState({ confirmList: responseJson.list, confirmLoad: true });
                 } else {
                     alert('데이터 로드에 실패했습니다.');
                 }
@@ -87,7 +87,7 @@ class Home extends Component {
             .then((response) => response.json())
             .then((responseJson) => {
                 if (responseJson.message == "SUCCESS") {
-                    this.setState({mysideConfirmList: responseJson.list, mysideLoad: true});
+                    this.setState({ mysideConfirmList: responseJson.list, mysideLoad: true });
                 } else {
                     alert('데이터 로드에 실패했습니다.');
                 }
@@ -147,38 +147,38 @@ class Home extends Component {
             [
                 {
                     text: 'Cancel', onPress: () => {
-                    return false
-                }, style: 'cancel'
+                        return false
+                    }, style: 'cancel'
                 },
                 {
                     text: 'OK', onPress: () => {
-                    try {
-                        fetch(PrivateAddr.getAddr() + "friend/rejectfriend?friendId=" + friendId, {
-                            method: 'DELETE', headers: {
-                                "Authorization": this.state.token.token,
-                                "Accept": "*/*",
-                            }
-                        })
-                            .then((response) => response.json())
-                            .then((responseJson) => {
-                                if (responseJson.message == "SUCCESS") {
-                                    alert("친구 요청을 거절했습니다");
-                                } else {
-                                    alert("친구 요청 거절 실패\n서버관리자에게 문의하세요");
-                                    return false;
+                        try {
+                            fetch(PrivateAddr.getAddr() + "friend/rejectfriend?friendId=" + friendId, {
+                                method: 'DELETE', headers: {
+                                    "Authorization": this.state.token.token,
+                                    "Accept": "*/*",
                                 }
                             })
-                            .catch((error) => {
-                                console.error(error);
-                            }).done(() => this.getConfirm());
-                    } catch (err) {
-                        alert('삭제실패 ' + err);
-                        return false;
+                                .then((response) => response.json())
+                                .then((responseJson) => {
+                                    if (responseJson.message == "SUCCESS") {
+                                        alert("친구 요청을 거절했습니다");
+                                    } else {
+                                        alert("친구 요청 거절 실패\n서버관리자에게 문의하세요");
+                                        return false;
+                                    }
+                                })
+                                .catch((error) => {
+                                    console.error(error);
+                                }).done(() => this.getConfirm());
+                        } catch (err) {
+                            alert('삭제실패 ' + err);
+                            return false;
+                        }
                     }
-                }
                 },
             ],
-            {cancelable: false}
+            { cancelable: false }
         )
     }
 
@@ -186,81 +186,79 @@ class Home extends Component {
         return (
             <View>
                 {(this.state.nicknameLoad == false || this.state.confirmLoad == false || this.state.mysideLoad == false) &&
-                <LoadingIcon/>
+                    <LoadingIcon />
                 }
                 <ScrollView contentContainerStyle={styles.homeWrapper}>
                     <Text style={styles.txt}>
                         '{this.state.nickname != undefined && this.state.nickname}'님 환영합니다!{'\n'}
                     </Text>
-                    <View style={styles.warningWrapper}>
-                        <Text style={styles.warningText}>
-                            ** 이 앱을 사용하는 도중에 발생하는
-                        </Text>
-                        <Text style={styles.warningText2}>
-                            모든 책임은 사용자 본인에게 있습니다 **
-                        </Text>
-                    </View>
-                    <View style={styles.hr}/>
+                    <View style={styles.hr} />
                     <TouchableOpacity
                         style={styles.postBtn}
                         underlayColor={'#000000'}
-                        onPress={() => Actions.main({goTo: 'post'})}
+                        onPress={() => Actions.main({ goTo: 'post' })}
                     >
                         <Text style={styles.txt}>커뮤니티 바로가기</Text>
                     </TouchableOpacity>
-                    <View style={styles.hr}/>
+                    <View style={styles.hr} />
                     <TouchableOpacity
                         style={styles.postBtn}
                         underlayColor={'#000000'}
-                        onPress={() => Actions.main({goTo: 'convert'})}
+                        onPress={() => Actions.main({ goTo: 'convert' })}
                     >
                         <Text style={styles.txt}>모의환전 바로가기</Text>
                     </TouchableOpacity>
-                    <View style={styles.hr}/>
+                    <View style={styles.hr} />
                     <View>
                         {(this.state.confirmLoad == true && this.state.confirmList.length != 0) &&
-                        <View style={styles.acceptWrapper}>
-                            <Text style={styles.listTitle}>친구 요청이 왔어요!</Text>
-                            {this.state.confirmList.map((list, i) => {
-                                return (
-                                    <View style={styles.rowViewWrapper} key={i}>
-                                        <Text style={styles.waitListText}>
+                            <View style={styles.acceptWrapper}>
+                                <Text style={styles.listTitle}>친구 요청이 왔어요!</Text>
+                                {this.state.confirmList.map((list, i) => {
+                                    return (
+                                        <View style={styles.rowViewWrapper} key={i}>
+                                            <Text style={styles.waitListText}>
+                                                {list.nickname}
+                                            </Text>
+                                            <View style={styles.rowView}>
+                                                <TouchableOpacity
+                                                    style={styles.confirmBtn}
+                                                    onPress={() => this.confirmRequest(list.id)}
+                                                >
+                                                    <Text style={styles.btnText}>수락</Text>
+                                                </TouchableOpacity>
+                                                <TouchableOpacity
+                                                    style={styles.confirmBtn}
+                                                    onPress={() => this.denyRequest(list.id)}
+                                                >
+                                                    <Text style={styles.btnText}>거절</Text>
+                                                </TouchableOpacity>
+                                            </View>
+                                        </View>
+                                    );
+                                })}
+                            </View>
+                        }
+                        <View style={styles.hr} />
+                        {(this.state.mysideLoad == true && this.state.mysideConfirmList.length != 0) &&
+                            <View>
+                                <Text style={styles.listTitle}>친구 요청 목록(대기중)</Text>
+                                {this.state.mysideConfirmList.map((list, i) => {
+                                    return (
+                                        <Text style={styles.listText} key={i}>
                                             {list.nickname}
                                         </Text>
-                                        <View style={styles.rowView}>
-                                            <TouchableOpacity
-                                                style={styles.confirmBtn}
-                                                onPress={() => this.confirmRequest(list.id)}
-                                            >
-                                                <Text style={styles.btnText}>수락</Text>
-                                            </TouchableOpacity>
-                                            <TouchableOpacity
-                                                style={styles.confirmBtn}
-                                                onPress={() => this.denyRequest(list.id)}
-                                            >
-                                                <Text style={styles.btnText}>거절</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                    </View>
-                                );
-                            })}
-                        </View>
+                                    );
+                                })}
+                            </View>
                         }
-                        <View style={styles.hr}/>
-                        {(this.state.mysideLoad == true && this.state.mysideConfirmList.length != 0) &&
-                        <View>
-                            <Text style={styles.listTitle}>친구 요청 목록(대기중)</Text>
-                            {this.state.mysideConfirmList.map((list, i) => {
-                                return (
-                                    <Text style={styles.listText} key={i}>
-                                        {list.nickname}
-                                    </Text>
-                                );
-                            })}
+                        <View style={styles.warningWrapper}>
+                            <Text style={styles.warningText}>
+                                ** 이 앱을 사용하는 도중에 발생하는
+                        </Text>
+                            <Text style={styles.warningText2}>
+                                모든 책임은 사용자 본인에게 있습니다 **
+                        </Text>
                         </View>
-                        }
-                        {/*<Text>Local Storage를 초기화하면 오류가 발생할수있음</Text>*/}
-                        {/*<Text style={styles.btn} onPress={this.clearStorage}>앱 Local Storage 삭제</Text>*/}
                     </View>
                 </ScrollView>
             </View>
@@ -288,13 +286,13 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         opacity: 0.8,
         fontSize: 15 * dpi,
-        textAlign:'left',
+        textAlign: 'left',
     },
     warningText2: {
         color: '#FFFFFF',
         opacity: 0.8,
         fontSize: 15 * dpi,
-        textAlign:'right',
+        textAlign: 'right',
     },
     hr: {
         opacity: 0.5,
